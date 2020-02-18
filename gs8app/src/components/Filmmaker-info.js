e53c75aab69e674c7334c5fef5be810a211722b2
Your Name <you@example.com>
2020-02-18 14:53:24 -0800

    finished contact and filmaker info component, fixed navbar issue, added routes

---
+++ gs8app/src/components/Filmmaker-info.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+const FilmmakerInfo = () => (
+    <div>
+        <div>
+        <button type="button">Introduction</button>
+        <button type="button">Filming Tips</button>
+        <button type="button">Cameras</button>
+        <button type="button">Links</button>
+        <button type="button">Rules</button>
+        <button type="button">Film Ideas</button>
+        <button type="button">Sound</button>
+        </div>
+
+        <div>
+            <h3><b><u>
+            Thank You for Participating in the Georgetown Super 8 Film Festival 2020 
+            </u></b></h3>
+            <br />
+
+            <p>Your time and willingness to be imaginative and share that creativity with the community is unique.  You didn’t have to spend money, rack your brain for an exciting idea, or create something that was purely for the fun of it, but you did, or will be soon, and that motivation makes you unique.  This event is about community, and it is your work, no matter what you choose to do, that will help to inspire the Duwamish Valley to continue being a little different and be recognized as the vibrant community we are all a part of.
+
+            <br /><br />
+            This is the second year of the reboot of Georgetown Super 8 Film Festival, which previously ran from 2006-2011.  There are both similarities and differences from the past project.  To help new and return filmmakers navigate these changes, this website has been created to guide you along the way.
+            <br /><br />
+            The most important thing to remember is not to let your filmmaking goals stress you out.  Just plan ahead and pace yourself.  With over 200 GS8 films made in the past, Georgetown and South Park have a lot of super 8 experts, so relax, have a beer/ coffee/ pizza slice/ vegan, veggie, or meat steak and ask the bartender, barista, or patron next to you how it is done.  If they don’t know, and our resources don’t answer your questions, shoot me an email, and I will be happy to get you on your way.
+            <br/> <br />
+            Thank you for particpating!
+            <br/>
+            <br />
+            Sincerely, <br />
+
+            Laura Wright <br />
+
+            GS8 Education and Archives Director <br />
+
+            gs8filmfest@gmail.com
+
+
+            </p>
+        </div>
+
+        <div>
+            <h3> Introduction </h3>
+            <h5><u>What is SUper 8?</u></h5>
+            <p>
+            Super 8 is a small format film that was popular for making home movies 35 years ago.  Many artists and beginning filmmakers used this medium because it was cheaper and easier to use than 16mm film and could provide a way to create a very low-budget independent film.  Filmmakers such as Spike Lee, JJ Abrams, and Steven Spielberg got their first taste of filmmaking using super 8, and its gritty look brought a renewed interest to the medium among artist and MTV videos responding to the rise of video use in the ’80s. 
+            <br /><br />
+            Super 8 is a film-based, light-sensitive medium, that must be processed after shooting before the filmmaker can see the results. This is a considerably different filmmaking experience than digital film, and requires attention to lighting, focusing, and framing without instant knowledge of what the camera is capturing.  To truly appreciate this unique film format, it is best to forget any Disney like expectations, embrace the beauty of film, and love the imperfections that happen when recording in this unique format. 
+            </p>
+        </div>
+
+        <h3>Filming Tips</h3>
+        <h4>All film due March 22nd</h4>
+
+        <div>
+            <h3>How to make a Super 8 Film</h3>
+            <iframe width="690" height="388" src="https://www.youtube.com/embed/N7OCSw9w-IA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <div>
+            <h3>Cameras</h3>
+            <iframe width="980" height="551" src="https://www.youtube.com/embed/BqLx6dyzVO0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <div>
+            <h3>Links</h3>
+        </div>
+
+        <div>
+            <h3>Film Ideas</h3>
+        </div>
+
+        <div>
+            <h3>Sound</h3>
+            <p>The sound cannot be synced with your film which means that having your sound fit perfectly with what is being shown is not possible, therefore shoot/ record accordingly.</p>
+            <h4>Due April 8th</h4>
+        </div>
+
+
+   </div>
+
+);
+
+export default FilmmakerInfo;
