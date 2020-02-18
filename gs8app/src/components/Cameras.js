649be9d78967a192354886759c9b078042528622
Joseph on Laptop <j.vincenzi@bellevuecollege.edu>
2020-02-18 15:11:45 -0800

    Joseph's Code to be reviewed for Sprint 1

--- gs8app/src/components/Cameras.js
+++ gs8app/src/components/Cameras.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 const parentDivStyle = {
-    textAlign: 'center'
+    textAlign: 'center',
 };
 
 const rowDivStyle = {
--- gs8app/src/components/Contact.js
+++ gs8app/src/components/Contact.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const Contact = () => (
     <div>
         <Router>
-            <p>This is gthe Contact page</p>
+            <p>This is the Contact page</p>
         </Router>
     </div>
 );
