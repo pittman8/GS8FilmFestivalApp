f123558ea24dc1c7049468d33d842afa998e748f
Joseph on Laptop <j.vincenzi@bellevuecollege.edu>
2020-02-19 22:21:31 -0800

    added missing import for BrowserRouter in /src/index.js

--- gs8app/src/index.js
+++ gs8app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
     <BrowserRouter>
