a1405492bb78fca1c0b34f4b3116cdf8c607bfe9
Hannah Pittman <pittman.h@hotmail.com>
2020-02-20 13:35:37 -0800

    shop stuff

--- gs8app/src/components/Shop.js
+++ gs8app/src/components/Shop.js
@@ -8,13 +8,19 @@ const Shop = () => (
         <Router>
             <h1>Purchase Your Film</h1>
             <h3>This page will no longer be accessible when registration ends January 31st at midnight.</h3>
-        <p>*insert film photos and cost here*</p>
+            <img src={require('../img/MyPost.jpg')} alt='The Georgetown Super 8 Film Festival logo' />
 
         <h2>Make a Tax-Deductible Donation</h2>
         <h3>Please Donate to Help Off-Set the cost of this project.</h3>
         <p>Mini Mart City Park is our fiscal sponsor.  Hit the button below to go to their webpage and scroll to the bottom  to the GS8 Donate Button</p>
         <h3>Thank you!</h3>
-            <Button variant="outlined" color="primary">Donate Now!
+            <Button
+                size="large"
+                color="primary"
+                variant="contained"
+                href="https://www.minimartcitypark.com/donate/"
+            >
+                Donate Now!
             </Button>
         <h3><b>An Important Note about the Cost</b></h3>
         <p>Previous GS8 participants may remember the $36 price to participate in 2010. Please understand that it is much more expensive to process super 8 film now, than it was 9 years ago.  There are no longer any local processing facilities and the cost of film has gone up. Unlike last year, we are now purchasing super 8 film from Kodak and having it processed at Cinelab in New England . We also have to pay for shipping 4 ways and a hard drive to store the digital versions of the films. The film fee of $100 only covers the cost of this process, not the purchase of cameras and their upkeep, instruction, office supplies, web services, or any other expenses.  If you are able, please make a tax deductible donation of $50, or more, (but anything is appreciated) to cover the rest of the expenses the project incurs, helping you make your film, and to ensure that everyone, regardless of their financial situation, is able to participate.
