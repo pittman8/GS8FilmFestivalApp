f7ad4d73bd48307b41483551eab61c2d9cce472e
Joseph on Laptop <j.vincenzi@bellevuecollege.edu>
2020-02-22 17:29:47 -0800

    matUi update for Participate.js

--- gs8app/src/components/Participate.js
+++ gs8app/src/components/Participate.js
@@ -1,79 +1,147 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+
+const buttonStyle = {};
+
+
+
+const parentDivStyle = {
+    textAlign: 'center',
+    display: 'inline-block',
+    width: '48em',
+};
+
+const cardStyle = {
+    textAlign: 'center',
+    padding: '15px',
+    margine: '15px',
+};
+
+const titleStyle = {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: '2.2em'
+};
+
+const regDivStyle = {
+    textAlign: 'center',
+    margin: '10px 0px',
+    padding: '5px',
+};
+
+const buttonDivStyle = {
+    textAlign: 'center',
+    display: 'inline-block',
+    center: 'top',
+    padding: '5px',
+    margine: '15px',
+    margin: '10px 0px',
+    width: '225px',
+};
+
+const pStyle = {
+    display: 'inline-block',
+    textAlign: 'center',
+    fontSize: '1.2em'
+};
+
+const rowDivStyle = {
+    align: 'left',
+};
+
 
 const Participate = () => (
-    <div>
-        <h1>Participate in GS8 Film Fes</h1>
-        <p>We are Glad you are here!</p>
-        <br />
-        <br />
-        <Button
-            size="large"
-            variant="contained"
-            href="/Register"
-        >
-            Registration - Start Here
-        </Button>
-        <br />
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Deadlines"
-        >
-            Deadlines & Events
-        </Button>
+    <div style={parentDivStyle}>
+        <Card style={cardStyle}>
+            <p style={titleStyle}>Participate in GS8 Film Festival</p>
+            <p style={pStyle}>We are Glad you are here!</p>
+            <div style={regDivStyle}>
+                <Button
+                    size="large"
+                    variant="contained"
+                    href="/Register"
+                >
+                    Registration<br/>Start Here
+                </Button>
+            </div>
 
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Shop"
-        >
-            Film Purchase
-        </Button>
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Filmmakers"
-        >
-            Filmmaker Info
-        </Button>
-        <br />
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Contact"
-        >
-            Contact
-        </Button>
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Rules"
-        >
-            Rules
-        </Button>
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Donate"
-        >
-            Donate
-        </Button>
-        <br />
-        <Button
-            size="large"
-            color="primary"
-            variant="contained"
-            href="/Cameras"
-        >
-            Camera Manuals
-        </Button>
+
+
+
+            <div style={rowDivStyle}>
+                <div style={buttonDivStyle}>
+                    <Button
+                        size="large"
+                        color="primary"
+                        variant="contained"
+                        href="/Deadlines"
+                        fullWidth='true'
+                    >
+                        Deadlines & Events
+                    </Button>
+                </div>
+
+                <div style={buttonDivStyle}>
+                    <Button
+                        size="large"
+                        color="primary"
+                        variant="contained"
+                        href="/Shop"
+                        fullWidth='true'
+                    >
+                        Film Purchase
+                    </Button>
+                </div>
+                <div style={buttonDivStyle}>
+                    <Button
+                        size="large"
+                        color="primary"
+                        variant="contained"
+                        href="/Filmmaker-info"
+                        fullWidth='true'
+                    >
+                        Filmmaker Info
+                    </Button>
+                </div>
+            </div>
+            <div>
+                <div style={buttonDivStyle}>
+                    <Button
+                        size="large"
+                        color="primary"
+                        variant="contained"
+                        href="/Contact"
+                        fullWidth='true'
+                    >
+                        Contact
+                    </Button>
+                </div>
+                <div style={buttonDivStyle}>
+                    <Button
+                        size="large"
+                        color="primary"
+                        variant="contained"
+                        href="/Rules"
+                        fullWidth='true'
+                    >
+                        Rules
+                    </Button>
+                </div>
+                <div style={buttonDivStyle}>
+                    <Button
+                        size="large"
+                        color="primary"
+                        variant="contained"
+                        href="/Donate"
+                        fullWidth='true'
+                    >
+                        Donate
+                    </Button>
+                </div>
+            </div>
+
+        </Card>
     </div>
 );
 
